17f8817e2ccc12173a46e6f05a00230471a2296e
jiajie <jiajie.ma@thoughtworks.com>
2020-10-16 15:09:14 +0800

    jiajie: finish user.test.js.

--- src/async/__test__/user.test.js
+++ src/async/__test__/user.test.js
@@ -1,11 +1,17 @@
 import { register } from "../user";
+import axios from "../../__mocks__/axios";
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // Arrange
+    const username = "abcdef";
+    const password = "123451qa";
     // Act
+    const result = await register(username, password);
     // Assert
+    expect(axios.post).toHaveBeenCalledTimes(1);
+    expect(result).toEqual({});
   });
 });
